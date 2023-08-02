d7d326b469431924aa70e0039ebfc61ed9f96682
AmletixQ <nazarovamridin308@gmail.com>
2023-08-02 14:58:04 +0300

    Confiure project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Home = () => {
+  return (
+    <div className="w-3/4 mt-2">
+      <h1>Main page</h1>
+    </div>
+  );
 };
 
-export default page;
+export default Home;
