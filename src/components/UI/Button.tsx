525eee0578c51e5e67ed43a8126878b982eb75a7
AmletixQ <nazarovamridin308@gmail.com>
2023-08-02 19:02:40 +0300

    Some fixes

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -7,7 +7,7 @@ const Button: FC<IProps> = (props) => {
   return (
     <button
       {...props}
-      className="border border-primary px-3 text-sm hover:bg-primary hover:text-white transition duration-200 cursor-pointer"
+      className="border border-primary px-4 py-[3px] text-sm hover:bg-primary hover:text-white transition duration-200 cursor-pointer"
     />
   );
 };
