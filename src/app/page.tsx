c6ac206925c9651dbfa988b5d88c1f74ea7970f7
AmletixQ <nazarovamridin308@gmail.com>
2023-08-02 16:09:10 +0300

    Add components to main page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+"use client";
+import TodoAddForm from "@/components/TodoAddForm";
+import TodoList from "@/components/TodoList";
+import Input from "@/components/UI/Input";
 
 const Home = () => {
   return (
-    <div className="w-3/4 mt-2">
-      <h1>Main page</h1>
+    <div className="w-2/4 mt-2 flex flex-col gap-2">
+      <Input placeholder="Search..." />
+      <hr />
+      <TodoAddForm />
+      <hr />
+      <TodoList />
     </div>
   );
 };
