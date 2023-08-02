d8e008adb42bbe0116f21a8b38a03a474a6112d4
AmletixQ <nazarovamridin308@gmail.com>
2023-08-02 16:04:52 +0300

    Some fixes & Configure

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,10 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      primary: "#9400d3",
+    extend: {
+      colors: {
+        primary: "#9400d3",
+      },
     },
   },
   plugins: [],
