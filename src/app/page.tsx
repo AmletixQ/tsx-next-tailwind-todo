e664f1b98a3a24de5830326ee3354ed9ab857f0b
AmletixQ <nazarovamridin308@gmail.com>
2023-08-03 10:13:48 +0300

    Add search, append data from localstorage at first rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,40 @@ import TodoAddForm from "@/components/TodoAddForm";
 import TodoList from "@/components/TodoList";
 import Input from "@/components/UI/Input";
 import { ITodoItem } from "@/types/todos";
-import { useState } from "react";
+import { getTodos } from "@/utils/DB";
+import { useEffect, useMemo, useState } from "react";
 
 const Home = () => {
-  const [todos, setTodos] = useState<ITodoItem[]>([
-    { id: 1, title: "NextJS", completed: true },
-    { id: 2, title: "Tailwind CSS", completed: false },
-  ]);
+  const data = getTodos();
+  const [search, setSearch] = useState("");
+  const [todos, setTodos] = useState<ITodoItem[]>([] as ITodoItem[]);
+
+  useEffect(() => {
+    if (data) setTodos(data);
+  }, []);
+
+  const filteredTodos = useMemo(() => {
+    if (search) {
+      return todos.filter((todo) => {
+        return todo.title.toLowerCase().includes(search.toLowerCase());
+      });
+    }
+
+    return todos;
+  }, [todos, search]);
+
   return (
     <div className="w-2/4 mt-2 flex flex-col gap-2">
-      <Input placeholder="Search..." />
+      <Input
+        type="text"
+        placeholder="Search..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <hr />
-      <TodoAddForm />
+      <TodoAddForm todos={todos} setTodos={setTodos} />
       <hr />
-      <TodoList todos={todos} setTodos={setTodos} />
+      <TodoList todos={filteredTodos} setTodos={setTodos} />
     </div>
   );
 };
