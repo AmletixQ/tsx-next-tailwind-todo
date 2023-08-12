d0fd70ad527b259de69a1f7bec9b15c63dad4220
AmletixQ <nazarovamridin308@gmail.com>
2023-08-12 20:18:33 +0300

    Add motions for todos and animations

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,8 @@
 import { ISetTodos, ITodoItem } from "@/types/todos";
+import { motion } from "framer-motion";
 import { saveData } from "@/utils/DB";
-import { Http2ServerRequest } from "http2";
-import { FC } from "react";
+import { FC, MouseEvent } from "react";
+import { Button, Card } from "@nextui-org/react";
 
 interface IProps extends ITodoItem, ISetTodos {}
 const TodoItem: FC<IProps> = ({ todos, setTodos, id, title, completed }) => {
@@ -20,22 +21,44 @@ const TodoItem: FC<IProps> = ({ todos, setTodos, id, title, completed }) => {
     saveData(newTodos);
   };
 
+  const deleteTodo = (e: MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+    saveData(newTodos);
+  };
+
   return (
-    <div
-      className={`border border-primary px-2 py-1 flex justify-between ${
-        completed ? "border-lime-800" : ""
-      } transition duration-200 hover:-translate-y-1 hover:shadow-lg hover:font-bold cursor-pointer select-none`}
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, x: -500 }}
+      transition={{ duration: 0.5 }}
       onClick={handleClick}
     >
-      <h2
-        className={`${
-          completed ? "text-lime-800" : ""
-        } transition-all duration-200`}
+      <Card
+        className={`px-2 py-1 flex flex-row justify-between items-center transition duration-200 hover:-translate-y-1 hover:shadow-lg hover:font-bold cursor-pointer select-none`}
       >
-        {title}
-      </h2>
-      {completed && <h2 className={"text-lime-800"}>Completed</h2>}
-    </div>
+        <h2
+          className={`${
+            completed ? "text-lime-800" : ""
+          } transition-all duration-200`}
+        >
+          {title}
+        </h2>
+        <div className="flex gap-1 items-center">
+          {completed && <h2 className={"text-lime-800"}>Completed</h2>}
+          <Button
+            size="sm"
+            variant="bordered"
+            color="success"
+            onClick={deleteTodo}
+          >
+            Delete
+          </Button>
+        </div>
+      </Card>
+    </motion.div>
   );
 };
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,24 +1,28 @@
 import { ISetTodos } from "@/types/todos";
 import { FC } from "react";
+
 import TodoItem from "./TodoItem";
+import { AnimatePresence } from "framer-motion";
 
 interface IProps extends ISetTodos {}
 const TodoList: FC<IProps> = ({ todos, setTodos }) => {
   return todos.length ? (
     <div className={"flex flex-col gap-2 transition"}>
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          id={todo.id}
-          title={todo.title}
-          completed={todo.completed}
-          todos={todos}
-          setTodos={setTodos}
-        />
-      ))}
+      <AnimatePresence>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            completed={todo.completed}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </AnimatePresence>
     </div>
   ) : (
-    <h2>Don&apos;t found todo</h2>
+    <h2 className="self-center text-2xl font-bold">Don&apos;t found todo</h2>
   );
 };
 
