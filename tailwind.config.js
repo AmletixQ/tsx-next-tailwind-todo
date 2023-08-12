01c652d0e53922c387ee0f986adb4e1703955804
AmletixQ <nazarovamridin308@gmail.com>
2023-08-12 20:16:21 +0300

    Add some rules to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#9400d3",
-      },
+    screens: {
+      sm: { max: "360px" },
+      md: { max: "768px" },
+      lg: { max: "1440px" },
     },
+    extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
