0e6c6a96349ecc1be6de4f92ea6382973115596d
AmletixQ <nazarovamridin308@gmail.com>
2023-09-26 21:15:23 +0300

    Finish build logic

--- src/store/todos.ts
+++ src/store/todos.ts
@@ -1,11 +1,31 @@
 import { IStoreTodos } from "@/types/todos";
+import { nanoid } from "nanoid";
 import { create } from "zustand";
 
 const useTodos = create<IStoreTodos>()((set) => ({
   todos: [],
-  createTodo: (title: string) => {},
-  deleteTodo: (id: number) => {},
-  updateTodo: (id: number) => {},
+  createTodo: (title: string) =>
+    set((state) => ({
+      todos: [...state.todos, { id: nanoid(), title, completed: false }],
+    })),
+
+  deleteTodo: (id: string) =>
+    set((state) => ({
+      todos: state.todos.filter((todo) => todo.id !== id),
+    })),
+
+  updateTodo: (id: string) =>
+    set((state) => ({
+      todos: state.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    })),
 }));
 
 export default useTodos;
