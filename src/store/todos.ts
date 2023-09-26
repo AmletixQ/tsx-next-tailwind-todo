80c2a5dcb7c84760c1a2b77514cf1131608f8ddd
AmletixQ <nazarovamridin308@gmail.com>
2023-09-26 21:58:25 +0300

    Add persist wrapper for storage

--- src/store/todos.ts
+++ src/store/todos.ts
@@ -1,31 +1,37 @@
 import { IStoreTodos } from "@/types/todos";
 import { nanoid } from "nanoid";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useTodos = create<IStoreTodos>()((set) => ({
-  todos: [],
-  createTodo: (title: string) =>
-    set((state) => ({
-      todos: [...state.todos, { id: nanoid(), title, completed: false }],
-    })),
+const useTodos = create<IStoreTodos>()(
+  persist(
+    (set) => ({
+      todos: [],
+      createTodo: (title: string) =>
+        set((state) => ({
+          todos: [...state.todos, { id: nanoid(), title, completed: false }],
+        })),
 
-  deleteTodo: (id: string) =>
-    set((state) => ({
-      todos: state.todos.filter((todo) => todo.id !== id),
-    })),
+      deleteTodo: (id: string) =>
+        set((state) => ({
+          todos: state.todos.filter((todo) => todo.id !== id),
+        })),
 
-  updateTodo: (id: string) =>
-    set((state) => ({
-      todos: state.todos.map((todo) => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            completed: !todo.completed,
-          };
-        }
-        return todo;
-      }),
-    })),
-}));
+      updateTodo: (id: string) =>
+        set((state) => ({
+          todos: state.todos.map((todo) => {
+            if (todo.id === id) {
+              return {
+                ...todo,
+                completed: !todo.completed,
+              };
+            }
+            return todo;
+          }),
+        })),
+    }),
+    { name: "todolists" },
+  ),
+);
 
 export default useTodos;
